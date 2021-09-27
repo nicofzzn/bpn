5a7a0f16f03f6c26bb149eb3e7982a11c700b7a7
nicofzzn <nicofaza975@gmail.com>
2021-09-27 11:46:32 +0700

    side menu

--- resources/views/Layout/index2.blade.php
+++ resources/views/Layout/index2.blade.php
@@ -91,23 +91,7 @@
       </div>
     </div>
 
-    <div class="quixnav">
-      <div class="quixnav-scroll">
-        <ul class="metismenu" id="menu">
-          <li class="nav-label first">Menu Berita</li>
-          <li><a href="/admin/berita" aria-expanded="false"><i class="ti-archive"></i><span class="nav-text ">Daftar
-                Berita</span></a>
-          </li>
-          <li><a href="/admin/buatberita" aria-expanded="false"><i class="ti-pencil-alt"></i><span class="nav-text">Buat
-                Berita</span></a>
-          </li>
-          <li class="nav-label">Menu Admin</li>
-          <li><a href="/logout" aria-expanded="false"><i class="ti-power-off"></i><span
-                class="nav-text">Logout</span></a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    @include('Layout.sidemenu')
     @yield('container')
 
 
---
+++ resources/views/Layout/sidemenu.blade.php
@@ -0,0 +1,16 @@
+<div class="quixnav">
+  <div class="quixnav-scroll">
+    <ul class="metismenu" id="menu">
+      <li class="nav-label first">Menu Berita</li>
+      <li><a href="/admin/berita" aria-expanded="false"><i class="ti-archive"></i><span class="nav-text ">Daftar
+            Berita</span></a>
+      </li>
+      <li><a href="/admin/buatberita" aria-expanded="false"><i class="ti-pencil-alt"></i><span class="nav-text">Buat
+            Berita</span></a>
+      </li>
+      <li class="nav-label">Menu Admin</li>
+      <li><a href="/logout" aria-expanded="false"><i class="ti-power-off"></i><span class="nav-text">Logout</span></a>
+      </li>
+    </ul>
+  </div>
+</div>
